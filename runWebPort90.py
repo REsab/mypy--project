8bfc82f831e9ec0e540e6a63ef2629642f3b4324
resab <resab@qq.com>
2026-05-01 15:55:28 +0800

    web file proxy

---
+++ runWebPort90.py
@@ -0,0 +1,11 @@
+import socketserver
+
+import http.server
+
+PORT = 90
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Serving at port", PORT)
+    httpd.serve_forever()
